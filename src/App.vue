2027b92b6e7a643c4bb8b5de2730af911d3e277f
Geocipriano <geocipriano28@outlook.com>
2020-08-08 15:16:53 -0300

    Alteração dinamica de Cor
    Alterar cor Dinamicamente usando two way databind.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <div id="app">
+  <div id="app" :style="{backgroundColor: pageColor}">
     <div class="titulo">
       <h1>Teste de Border Radius</h1>
     </div>
-    <div class="inputTop">
-      <input type="text" v-model="color">
-      <input type="text" v-model="color">
+    <div class="inputText">
+        <div>
+          <p>Conteúdo do Texto </p>
+          <input type="text" v-model="text">
+        </div>
     </div>
+    <div class="inputColor">
+      <div>
+        <p>Cor do Texto </p>
+        <input type="text" v-model="textColor">
+      </div>
+      <div>
+        <p>Cor da Bloco </p>
+        <input type="text" v-model="backgroundColor">
+      </div>
+      <div>
+        <p>Cor da Margem </p>
+        <input type="text" v-model="borderColor">
+      </div>
+    </div>
+
     <div class="bolder">
-      <div :style="{color: color}">Banana</div>
-    </div>
-    <div class="inputBottom">
-      <input type="text" v-model="color">
-      <input type="text" v-model="color">
+      <div class="box" :style="{backgroundColor : backgroundColor, borderColor : borderColor}">
+        <div :style="{color: textColor}">{{text}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,28 +37,62 @@ export default {
   name: "App",
   data() {
     return {
-      color : 'blue'
+      pageColor:'',
+      borderColor:'',
+      text : '',
+      textColor: '',
+      color : 'blue',
+      backgroundColor: 'red',
     };
   }
 };
 </script>
 
 <style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+}
 .titulo {
   display: flex;
   justify-content: center;
+  padding: 30px;
 }
-.inputTop {
+.inputText {
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  align-items: center;
+}
+.inputText div input{
+  width: 500px;
+}
+.inputColor {
+  display: flex;
+  justify-content: center;
+  padding: 15px;
+}
+.inputColor div {
+  padding: 15px;
+  margin: 0 15px;
+}
+.inputColor input {
+  width: 100px;
 }
 .bolder {
   display: flex;
   justify-content: center;
 }
-.inputBottom {
+.box {
+  width: 500px;
+  height: 300px;
+  border: 3px solid black;
+  margin: 30px;
   display: flex;
-  justify-content: space-around;
+  align-items: center;
+  justify-content: center;
 }
+
 
 </style>
